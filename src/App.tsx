5e58ada2f2a1013f255213afe8e84ca683dbf23a
heony704 <heony704@gmail.com>
2024-07-23 11:43:04 +0900

    feat: page 추가 및 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AddSponsorPage from './pages/AddSponsorPage';
+import LandingPage from './pages/LandingPage';
+import ListPage from './pages/ListPage';
+import MyPage from './pages/MyPage';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
     <>
-      <div>App</div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/addsponsor" element={<AddSponsorPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
