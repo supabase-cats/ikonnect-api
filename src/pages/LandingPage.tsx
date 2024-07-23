3482b948560ad403557ce97e9a40f29ea1716611
heony704 <heony704@gmail.com>
2024-07-23 12:11:50 +0900

    feat: landing page 구현

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button';
+import { useNavigate } from 'react-router-dom';
+
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-40">
+      <h1 className="text-5xl font-bold tracking-wider">I KONNECT</h1>
+      <Button onClick={() => navigate('/list')}>Connect !</Button>
+    </main>
+  );
 }
