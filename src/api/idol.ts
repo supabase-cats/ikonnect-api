2c0ff809c715c649e04524cd43cc2ae0845bdf50
heony704 <heony704@gmail.com>
2024-08-02 11:27:36 +0900

    feat: 조공 추가 페이지 아이돌 검색 기능 추가

--- src/api/idol.ts
+++ src/api/idol.ts
@@ -18,3 +18,16 @@ export async function getIdols(): Promise<Idol[]> {
 
   return data;
 }
+
+export async function searchIdol(keyword: string): Promise<Idol[]> {
+  const { data, error } = await supabase
+    .from(tableName)
+    .select('*')
+    .or(`name.ilike.%${keyword}%,group.ilike.%${keyword}%`); // 'name' 또는 'description' 필드에서 'John'이 포함된 데이터 검색
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
--- src/pages/AddSponsorPage.tsx
+++ src/pages/AddSponsorPage.tsx
@@ -1,4 +1,4 @@
-import { getIdols } from '@/api/idol';
+import { getIdols, searchIdol } from '@/api/idol';
 import DatePicker from '@/components/DatePicker';
 import IdolAvatar from '@/components/IdolAvatar';
 import Swiper from '@/components/Swiper';
@@ -6,11 +6,14 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type Idol = Awaited<ReturnType<typeof getIdols>>[number];
 
 export default function AddSponsorPage() {
   const [idols, setIdols] = useState<Idol[]>([]);
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchIdols = async () => {
@@ -21,6 +24,15 @@ export default function AddSponsorPage() {
     fetchIdols();
   }, []);
 
+  useEffect(() => {
+    const fetchIdols = async () => {
+      const data = await searchIdol(searchKeyword);
+      setIdols(data);
+    };
+
+    fetchIdols();
+  }, [searchKeyword]);
+
   return (
     <main className="m-auto flex max-w-[1200px] flex-col px-10 pb-20 pt-1">
       <div className="flex items-center justify-between">
@@ -28,20 +40,30 @@ export default function AddSponsorPage() {
         <Input
           className="ml-5 max-w-[500px]"
           placeholder="조공할 아이돌을 검색해보세요"
+          value={searchKeyword}
+          onChange={(e) => {
+            setSearchKeyword(e.target.value);
+          }}
         />
       </div>
-      <div className="mt-8">
-        <Swiper>
-          {idols.map((idol) => (
-            <div
-              key={idol.id}
-              className="flex select-none flex-col items-center justify-start"
-            >
-              <IdolAvatar image={idol.image} alt={idol.name} />
-              <p className="mt-2">{idol.name}</p>
-            </div>
-          ))}
-        </Swiper>
+      <div className="mt-8 h-[130px]">
+        {idols.length > 0 ? (
+          <Swiper>
+            {idols.map((idol) => (
+              <div
+                key={idol.id}
+                className="flex select-none flex-col items-center justify-start"
+              >
+                <IdolAvatar image={idol.image} alt={idol.name} />
+                <p className="mt-2">{idol.name}</p>
+              </div>
+            ))}
+          </Swiper>
+        ) : (
+          <div className="flex h-full w-full items-center justify-center">
+            결과가 없습니다
+          </div>
+        )}
       </div>
       <div className="mt-10 flex flex-col gap-8">
         <Input placeholder="제목" />
@@ -58,7 +80,7 @@ export default function AddSponsorPage() {
         </div>
       </div>
       <div className="mt-5 flex justify-center gap-3">
-        <Button>취소</Button>
+        <Button onClick={() => navigate('/list')}>취소</Button>
         <Button>등록</Button>
       </div>
     </main>
